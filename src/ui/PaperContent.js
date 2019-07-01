5f0d453492cc3f48412d923305d1fb322b3c5d26
Gustavo Guichard <gustavoguichard@gmail.com>
2019-06-30 23:44:26 -0300

    Products section

--- src/ui/PaperContent.js
+++ src/ui/PaperContent.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Paper, Container, Box } from '@material-ui/core'
+import { useIsMobile } from 'utils/responsive'
 
 const PaperContent = ({ children, overlap = true, ...props }) => {
+  const isMobile = useIsMobile()
   const marginBottom = overlap ? '-3rem' : 0
   const marginTop = overlap ? '-5rem' : 0
   return (
     <Container maxWidth="lg" css={{ zIndex: 3, position: 'relative' }}>
       <Paper css={{ marginBottom, marginTop }} elevation={20}>
-        <Box my={4} p={5}>
+        <Box my={4} py={5} px={isMobile ? 2 : 5}>
           {children}
         </Box>
       </Paper>
