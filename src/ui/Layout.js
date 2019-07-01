9a2d636968db25964d49fd367c2adc86dfd590cc
Gustavo Guichard <gustavoguichard@gmail.com>
2019-07-01 15:37:02 -0300

    Remove Croods reference

--- src/ui/Layout.js
+++ src/ui/Layout.js
@@ -1,17 +1,13 @@
 import { memo } from 'react'
-import { useCroods } from 'croods'
 import Header from 'src/ui/Header'
 import Footer from 'src/ui/Footer'
 
-const Layout = ({ children, variant = 'primary', stickBar }) => {
-  const [state] = useCroods({ name: 'produtos', fetchOnMount: true })
-  return (
-    <>
-      <Header variant={variant} stick={stickBar} />
-      <main css={{ flex: 1 }}>{children}</main>
-      <Footer varian={variant} />
-    </>
-  )
-}
+const Layout = ({ children, variant = 'primary', stickBar }) => (
+  <>
+    <Header variant={variant} stick={stickBar} />
+    <main css={{ flex: 1 }}>{children}</main>
+    <Footer varian={variant} />
+  </>
+)
 
 export default memo(Layout)
