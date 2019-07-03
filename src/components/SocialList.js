92698e555caa8ab8014ece16bc73877cfa6fb9af
Gustavo Guichard <gustavoguichard@gmail.com>
2019-07-03 16:12:08 -0300

    Refactor

--- src/components/SocialList.js
+++ src/components/SocialList.js
@@ -3,51 +3,48 @@ import { FiFacebook, FiInstagram } from 'react-icons/fi'
 import ButtonLink from 'src/components/ButtonLink'
 import theme from 'src/ui/theme'
 
+const icons = {
+  facebook: props => <FiFacebook {...props} />,
+  instagram: props => <FiInstagram {...props} />,
+  whatsapp: props => <FaWhatsapp {...props} />,
+}
+
+const css = {
+  margin: theme.spacing(),
+  transition: 'all .3s',
+  '&:hover': {
+    color: theme.palette.secondary.main,
+  },
+}
+
 const Icon = ({
-  Component,
+  site,
   children,
   color = theme.pallete.primary.contrastText,
   size = 20,
   ...props
 }) => (
   <ButtonLink size="small" icon {...props} title={children} target="blank">
-    <Component
-      css={{
-        margin: theme.spacing(),
-        transition: 'all .3s',
-        '&:hover': {
-          color: theme.palette.secondary.main,
-        },
-      }}
-      size={size}
-      color={color}
-    />
+    {icons[site]({ css, size, color })}
   </ButtonLink>
 )
 
 const SocialList = ({ className, ...props }) => {
   return (
     <div className={`social-list ${className}`}>
-      <Icon
-        Component={FaWhatsapp}
-        href="https://wa.me/5548991039557"
-        color={theme.palette.secondary.light}
-        {...props}
-      >
+      <Icon site="whatsapp" href="https://wa.me/5548991039557" {...props}>
         Fale conosco por whatsapp
       </Icon>
       <Icon
-        Component={FiInstagram}
+        site="instagram"
         href="https://instagram.com/vidanatural.eco"
-        color="#B05CFE"
         {...props}
       >
         Ir para nosso Instagram
       </Icon>
       <Icon
-        Component={FiFacebook}
+        site="facebook"
         href="http://facebook.com/vidanatural.eco"
-        color="#0082FB"
         {...props}
       >
         Ir para nosso Facebook
