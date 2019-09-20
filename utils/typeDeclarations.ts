41ea6d0df3c95d7e9255dac09e8fae8e6354d270
Gustavo Guichard <gustavoguichard@gmail.com>
2019-09-20 20:22:45 -0300

    Add global state system

---
+++ utils/typeDeclarations.ts
@@ -0,0 +1,21 @@
+export interface GlobalState {
+  [key: string]: any
+}
+
+export interface Action {
+  (t: Store, ...args: any[]): any
+}
+
+export interface Actions {
+  [key: string]: Action | Actions
+}
+
+export type Listener = [string | undefined, React.Dispatch<any>]
+
+export interface Store {
+  setState: Function
+  setGlobalState?: Function
+  actions?: Actions
+  state: GlobalState
+  listeners?: Listener[]
+}
