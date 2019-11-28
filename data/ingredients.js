92fc8fdeeedfce876bb604b4a421519d10b4772b
Gustavo Guichard <gustavoguichard@gmail.com>
2019-11-28 15:41:28 -0300

    Update some ingredient descriptions

--- data/ingredients.js
+++ data/ingredients.js
@@ -4,13 +4,13 @@ export default [
   {
     name: 'Água Purificada',
     inci: 'Aqua',
-    description: 'Essa dispensa apresentação',
+    description: 'Essa dispensa apresentação.',
   },
   {
     name: 'Manteiga de Karité',
     inci: 'Butyrospermum Parkii',
     showHome: true,
-    description: 'Ingrediente orgânico',
+    description: 'Ingrediente orgânico.',
     url: '700875/BUTYROSPERMUM_PARKII_%28SHEA_BUTTER%29/',
   },
   {
@@ -51,163 +51,187 @@ export default [
   {
     name: 'Vitamina E',
     inci: 'Tocopheryl Acetate',
-    description: '',
+    description:
+      'Por suas propriedades cicatrizantes e antioxidantes, a vitamina E vem sendo usada para tratamentos da pele. Entre os benefícios do uso da vitamina E para a pele estão: prevenção do envelhecimento da pele, prevenção de rugas, melhora a cicatrização, aumenta a firmeza da pele, já que participa da formação do colágeno, age como lubrificante e hidratante potente para a pele, reduz os danos causados pelos raios ultravioleta, etc.',
     url: '706569/TOCOPHERYL_ACETATE/',
   },
   {
     name: 'Óleo Essencial de Canela',
     inci: 'Cinnamomum Zeylanicum Leaf Oil',
-    description: '',
+    description:
+      'O óleo essencial de canela pode ser utilizado para o tratamento de muitas doenças, principalmente dos problemas de pele como as erupções cutâneas, a acne e as infecções, pois ele possui propriedades antimicrobianas.',
     url: '726690/CINNAMOMUM_ZEYLANICUM_%28CINNAMON%29_LEAF_OIL/',
   },
   {
     name: 'Óleo Essencial de Tea Tree',
     inci: 'Melaleuca Alternifolia Leaf Extract',
     showHome: true,
-    description: 'Ingrediente orgânico',
+    description:
+      'Ingrediente orgânico, poderoso antifúngico. O OE de Tea tree atua sobre brotoejas, picadas de insetos, micoses, dermatites, aftas, "sapinhos", piolhos, furúnculos, sarna, dores de garganta, abscessos, acne e pé-de-atleta.',
     url: '720554/MELALEUCA_ALTERNIFOLIA_%28TEA_TREE%29_LEAF_EXTRACT/',
   },
   {
     name: 'Óleo Essencial de Lavanda',
     inci: 'Lavandula Angustifolia Extract',
     showHome: true,
-    description: '',
+    description:
+      'Além de muitos efeitos conhecidos, na pele, o OE de lavanda destaca-se por ser um grande regenerador das células cutâneas. É bastante utilizado em tratamentos de acne, peles desidratadas e maltratadas. Também utilizado como método para tratar queimaduras, inflamações, psoríases e feridas.',
     url: '703484/LAVANDULA_ANGUSTIFOLIA_%28LAVENDER%29_EXTRACT/',
   },
   {
     name: 'Óleo Essencial de Lavandin',
     inci: 'Lavandula Hybrida Oil',
-    description: '',
+    description:
+      'É uma espécie híbrida de Lavanda, dentre os tipos de lavanda, é o mais eficaz em ações: anti-fúngicas, antivirais, bactericidas e imunoestimulantes.',
     url: '703490/LAVANDULA_HYBRIDA_%28LAVANDIN%29_OIL/',
   },
   {
     name: 'Óleo Essencial de Capim Limão',
     inci: 'Cymbopogon Flexuosos Leaf Oil',
-    description: '',
+    description:
+      'Possui uma ampla variedade de aplicações, como por exemplo: anti-inflamatório, analgésico, antisséptico, antipirético, antioxidante, antimicrobiano, fungicida, etc.',
     url: '701748/CYMBOPOGON_%28LEMON_GRASS%29_OIL/',
   },
   {
     name: 'Óleo Essencial de Hortelã',
     inci: 'Mentha Piperita (Peppermint) Oil',
-    description: '',
+    description: 'Além do conhecido e amado aroma, possui ação bactericída.',
     url: '703844/MENTHA_PIPERITA_%28PEPPERMINT%29_OIL/',
   },
   {
     name: 'Óleo Resina Alecrim',
     inci: 'Rosmarinus Officinalis',
-    description: '',
+    description:
+      'Conhecido por ser um dos mais efetivos antioxidantes naturais.',
     url: '705663/ROSMARINUS_OFFICINALIS_%28ROSEMARY%29_OLEORESIN/',
   },
   {
     name: 'Óleo de Girassol ',
     inci: 'Helianthus Annuus (Sunflower) Oil',
-    description: '100% vegetal e prensado a frio',
+    description:
+      '100% vegetal e prensado a frio, mantendo suas propriedades. É constituido basicamente de ácidos graxos insaturados (ômegas 3, 6 e 9) e vitamina E.',
     url: '702804/HELIANTHUS_ANNUUS_%28SUNFLOWER%29_OIL/',
   },
   {
     name: 'Hidróxido de Magnésio',
     inci: 'Magnesium Hydroxide',
-    description: '',
+    description:
+      'Base para o conhecido Leite de Magnésia, que é utilizado como antiácido em nossa fórmula, regulando o PH do suor e assim impedindo a proliferação de bactérias nas axilas.',
     url: '720445/MAGNESIUM_HYDROXIDE/',
   },
   {
     name: 'Goma de Celulose',
     inci: 'Cellulose Gum',
-    description: '',
+    description:
+      'Agente estabilizador e espessante, é extraída a partir da polpa da madeira e celulose purificada.',
     url: '701190/CELLULOSE_GUM/',
   },
   {
     name: 'Glicerina Vegetal',
     inci: 'Glycerin',
-    description: '',
+    description:
+      'Possui propriedades emolientes, lubrificantes, umectantes e hidratantes que contribuem para absorção da água na pele e nos cabelos, promovendo hidratação e maciez.',
     url: '706851/VEGETABLE_GLYCERIN/',
   },
   {
     name: 'Óleo de Rosa Mosqueta',
     inci: 'Rosa Canina Fruit Oil',
     showHome: true,
-    description: '',
+    description:
+      'Reverenciada pelos egípcios, maias e norte-americanos nativos por suas propriedades terapêuticas, o óleo de Rosa Mosqueta oferece uma forma natural de combater rugas, desacelerar o envelhecimento e se atingir uma pele saudável. Estudos clínicos demonstram que ela também tem qualidades antibacterianas, antivirais e antifúngicas.',
     url: '705579/ROSA_CANINA_%28DOG_ROSE%29_FRUIT_OIL/',
   },
   {
     name: 'Palmarosa',
     inci: 'Cymbopogon Martini',
-    description: '',
+    description:
+      'O óleo essencial de Palmarosa é extraído por hidratação ou destilação a vapor, que produz um óleo  com um odor doce, floral e rosado, com nuances frutadas, especiadas e amadeiradas. Alguns dos usos do óleo de palmarosa incluem: tratamento de manchas na pele, revitalização e manutenção da flexibilidade e outros danos causados pelo envelhecimento, tratamento de fungos, entre outros.',
     url: '701749/CYMBOPOGON_MARTINI_%28PALMAROSA%29/',
   },
   {
     name: 'Copaíba',
     inci: 'Copaifera Officinalis Resin',
-    description: '',
+    description:
+      'O óleo de copaíba foi apelidado de "bálsamo da Amazônia" devido às suas qualidades. Possui componentes de ação anti-inflamatória, antifúngica, antiedêmica, antiviral, antiúlcera, entre outros... ufa!',
     url: '701635/COPAIFERA_OFFICINALIS_%28BALSAM_COPAIBA%29_RESIN/',
   },
   {
     name: 'Café Verde',
     inci: 'Coffea Arabica',
-    description: '',
+    description:
+      'Possui propriedades tonificantes, suavizantes e antissépticas e é muito utilizado em tratamentos de beleza corporais e faciais. É rico em ácidos graxos insaturados, como o ácido linoleico, que atua na cura de dermatites e alívio de eczemas. Os ácidos graxos promovem, também, a hidratação da pele e proteção contra raios UV.',
     url: '701591/COFFEA_ARABICA_%28ARABIAN_COFFEE%29/',
   },
   {
     name: 'Óleo de Oliva',
     inci: 'Olea Europaea',
     showHome: true,
-    description: '',
+    description:
+      'Rico em vitaminas A, D, E e K e antioxidantes, o azeite colabora para o combate aos radicais livres. Sendo assim, ajuda na prevenção do envelhecimento precoce da pele.',
     url: '704248/OLEA_EUROPAEA_%28OLIVE%29/',
   },
   {
     name: 'Lactato de Sódio',
     inci: 'Sodium Lactate',
-    description: '',
+    description:
+      'O lactato de sódio é o sal de sódio do ácido láctico e apresenta um sabor salino suave. É produzido por fermentação de uma fonte de açúcar, como milho ou beterraba.',
     url: '706084/SODIUM_LACTATE/',
   },
   {
     name: 'Argila Verde',
     inci: 'Montmorillonite',
     showHome: true,
-    description: '',
+    description:
+      'A argila é famosa pelos seus benefícios no cuidado com a pele, sendo comumente usada em tratamentos há milhares de anos. Os egípcios, por exemplo, já usavam esse componente nas suas mumificações.',
     url: '704015/MONTMORILLONITE/',
   },
   {
     name: 'Juá em Pó',
     inci: 'Zizyphus Joazeiro Bark Extract',
     showHome: true,
-    description: '',
+    description:
+      'Simplesmente a casca de uma árvore (Juazeiro) em pó! O juá é um antiséptico e saponáceo natural.',
     url: '724946/ZIZYPHUS_JOAZEIRO_BARK_EXTRACT/',
   },
   {
     name: '',
     inci: 'Caprylhydroxamic Acid',
-    description: '',
+    description:
+      'É ideal para produtos de cuidados pessoais onde uma reivindicação sem paraben ou livre de conservante é necessária. Pode ser usado como uma alternativa às misturas conservantes tradicionais que são vistas como indesejáveis ​​pelo consumidor. ',
     url: '862573/CAPRYLHYDROXAMIC_ACID/',
   },
   {
     name: '',
     inci: 'Caprylyl Glycol',
-    description: '',
+    description:
+      'É um álcool derivado do ácido caprílico, que é  um ácido gordo natural encontrado nos óleos de palma e de coco. Considerado um conservante "preservative free", é uma ótima opção para sua cosmética natural e orgânica.',
     url: '701067/CAPRYLYL_GLYCOL/',
   },
   {
     name: 'Etilhexilglicerina',
     inci: 'Ethylhexylglycerin',
-    description: '',
+    description:
+      'A etilhexilglicerina, ou octoxiglicerina, é um éter glicerílico que é comumente usado como parte de um sistema conservante em preparações cosméticas, comumente usado em cosméticos livres de parabeno.',
     url: '702352/ETHYLHEXYLGLYCERIN/',
   },
   {
     name: '',
     inci: 'Aminomethyl Propanol',
-    description: '',
+    description:
+      'Poduto ultilizado em formulações de gel - neutralizante - também usado para aumentar o ph das formulações. É um composto orgânico com a fórmula H₂NC (CH2) ₂CH2OH. É um líquido incolor classificado como alcanolamina. É um tampão útil e um precursor de vários outros compostos orgânicos.',
     url: '700348/AMINOMETHYL_PROPANOL/',
   },
   {
     name: '',
     inci: 'Acrylates/C10-30 Alkyl Acrylate Crosspolymer',
-    description: '',
+    description:
+      'Agente espessante e emulsificante. O polímero cruzado de acrilato de alquil acrilato/C10-30 é um copolímero de vários ingredientes. Os polímeros geralmente são orgânicos e formados a partir de grandes cadeias de monômeros, que são moléculas que podem se ligar umas nas outras.',
     url: '700138/ACRYLATES%3B%3B_C10-30_ALKYL_ACRYLATE_CROSSPOLYMER/',
   },
   {
     name: 'Gel de Aloe Vera',
     inci: 'Aloe Vera Gel',
-    description: '',
+    description: 'Simplesmente: Gel de babosa!',
     url: '700269/ALOE_BARBADENSIS_%28ALOE_VERA%29_GEL/',
   },
 ]
