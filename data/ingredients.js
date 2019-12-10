dd672cbff53f44c45b527e2321caab7562e22caf
Gustavo Guichard <gustavoguichard@gmail.com>
2019-12-10 13:52:15 -0300

    Update ingredients

--- data/ingredients.js
+++ data/ingredients.js
@@ -4,21 +4,21 @@ export default [
   {
     name: 'Água Purificada',
     inci: 'Aqua',
-    description: 'Essa dispensa apresentação.',
+    description: '<3 Água <3 sem sais minerais e matérias orgânicas.',
   },
   {
     name: 'Manteiga de Karité',
     inci: 'Butyrospermum Parkii',
     showHome: true,
     hasIcon: true,
-    description: 'Ingrediente orgânico.',
+    description: 'Super hidratante!',
     url: '700875/BUTYROSPERMUM_PARKII_%28SHEA_BUTTER%29/',
   },
   {
     name: 'Óleo de Coco Palmiste',
     inci: 'Elaeis Guineensis (Palm) Kernel Oil',
     description:
-      'Ingrediente orgânico de origem brasileira, proveniente do dendezeiro.',
+      'Ingrediente orgânico e produzido no Brasil. O óleo é extraído da amêndoa do dende por métodos físicos (prensagem mecânica) sem uso de solventes ou outras substâncias químicas. O refino é realizado de forma natural (processo físico) sem uso de insumos químicos.',
     url: '704405/PALM_KERNEL_OIL/',
   },
   {
@@ -26,13 +26,14 @@ export default [
     inci: 'Elaeis Guineensis (Palm) Oil',
     showHome: true,
     description:
-      'Ingrediente orgânico de origem brasileira, proveniente do dendezeiro.',
+      'Ingrediente orgânico e produzido no Brasil. O óleo é extraído da amêndoa do dende por métodos físicos (prensagem mecânica) sem uso de solventes ou outras substâncias químicas. O refino é realizado de forma natural (processo físico) sem uso de insumos químicos.',
     url: '702238/ELAEIS_GUINEENSIS_%28AFRICAN_OIL_PALM%29_OIL/',
   },
   {
     name: 'Bicarbonato de Sódio',
     inci: 'Sodium Bicarbonate',
-    description: '',
+    description:
+      'No desodorante, é um inibidor de bactérias que causam o mau cheiro. Ele regula o PH da axila, reduzindo a acidez e a proliferação de bactérias. ',
     url: '705994/SODIUM_BICARBONATE/',
   },
   {
@@ -46,14 +47,15 @@ export default [
   {
     name: 'Cera de Carnaúba',
     inci: 'Copernicia Cerifera Cera',
-    description: '',
+    description:
+      'Cera vegetal que dá consistência ao desodorante. Também atua como hidratante.',
     url: '701122/COPERNICIA_CERIFERA_%28CARNAUBA%29_WAX/',
   },
   {
     name: 'Vitamina E',
     inci: 'Tocopheryl Acetate',
     description:
-      'Por suas propriedades cicatrizantes e antioxidantes, a vitamina E vem sendo usada para tratamentos da pele. Entre os benefícios do uso da vitamina E para a pele estão: prevenção do envelhecimento da pele, prevenção de rugas, melhora a cicatrização, aumenta a firmeza da pele, já que participa da formação do colágeno, age como lubrificante e hidratante potente para a pele, reduz os danos causados pelos raios ultravioleta, etc.',
+      'Conservante natural. Por suas propriedades cicatrizantes e antioxidantes, a vitamina E vem sendo usada para tratamentos da pele.',
     url: '706569/TOCOPHERYL_ACETATE/',
   },
   {
@@ -69,7 +71,7 @@ export default [
     showHome: true,
     hasIcon: true,
     description:
-      'Ingrediente orgânico, poderoso antifúngico. O OE de Tea tree atua sobre brotoejas, picadas de insetos, micoses, dermatites, aftas, "sapinhos", piolhos, furúnculos, sarna, dores de garganta, abscessos, acne e pé-de-atleta.',
+      'Poderoso antisséptico. Bactericida, antifúngico, revigora o sistema imunológico e ajuda o corpo a combater infecções. Elimina o mau cheiro e auxilia na higiene bucal. Indispensável!',
     url: '720554/MELALEUCA_ALTERNIFOLIA_%28TEA_TREE%29_LEAF_EXTRACT/',
   },
   {
@@ -78,14 +80,14 @@ export default [
     showHome: true,
     hasIcon: true,
     description:
-      'Além de muitos efeitos conhecidos, na pele, o OE de lavanda destaca-se por ser um grande regenerador das células cutâneas. É bastante utilizado em tratamentos de acne, peles desidratadas e maltratadas. Também utilizado como método para tratar queimaduras, inflamações, psoríases e feridas.',
+      'Regenera a pele, estimulando o crescimento de novas células saudáveis. Hidrata, suaviza e trata a pele. Protege e repara. Cicatrizante e bactericida é   usado para tratar queimaduras, inflamações, psoríases e feridas.',
     url: '703484/LAVANDULA_ANGUSTIFOLIA_%28LAVENDER%29_EXTRACT/',
   },
   {
     name: 'Óleo Essencial de Lavandin',
     inci: 'Lavandula Hybrida Oil',
     description:
-      'É uma espécie híbrida de Lavanda, dentre os tipos de lavanda, é o mais eficaz em ações: anti-fúngicas, antivirais, bactericidas e imunoestimulantes.',
+      'Propriedades muito semelhantes as da Lavanda, porém ainda mais terapêutico. Mais eficaz em ações: anti-fúngicas, antivirais, bactericidas e imunoestimulantes.',
     url: '703490/LAVANDULA_HYBRIDA_%28LAVANDIN%29_OIL/',
   },
   {
@@ -93,21 +95,21 @@ export default [
     inci: 'Cymbopogon Flexuosos Leaf Oil',
     hasIcon: true,
     description:
-      'Possui uma ampla variedade de aplicações, como por exemplo: anti-inflamatório, analgésico, antisséptico, antipirético, antioxidante, antimicrobiano, fungicida, etc.',
+      'Excelente no tratamento de manchas. Revitaliza a pele e protege dos danos causados pelo envelhecimento.',
     url: '701748/CYMBOPOGON_%28LEMON_GRASS%29_OIL/',
   },
   {
     name: 'Óleo Essencial de Hortelã',
     inci: 'Mentha Piperita (Peppermint) Oil',
     hasIcon: true,
-    description: 'Além do conhecido e amado aroma, possui ação bactericída.',
+    description: 'Além do conhecido e amado aroma, possui ação bactericida.',
     url: '703844/MENTHA_PIPERITA_%28PEPPERMINT%29_OIL/',
   },
   {
     name: 'Óleo Resina Alecrim',
     inci: 'Rosmarinus Officinalis',
     description:
-      'Conhecido por ser um dos mais efetivos antioxidantes naturais.',
+      'Conhecido por ser um dos mais efetivos antioxidantes / conservantes naturais. É um extrato natural das folhas do Alecrim.',
     url: '705663/ROSMARINUS_OFFICINALIS_%28ROSEMARY%29_OLEORESIN/',
   },
   {
@@ -115,14 +117,22 @@ export default [
     inci: 'Helianthus Annuus (Sunflower) Oil',
     hasIcon: true,
     description:
-      '100% vegetal e prensado a frio, mantendo suas propriedades. É constituido basicamente de ácidos graxos insaturados (ômegas 3, 6 e 9) e vitamina E.',
+      '100% vegetal e prensado a frio, o que preserva as vitaminas e nutrientes presentes nas sementes. É um óleo reparador e suavizador da pele. Óleo leve e fácil absorção. ',
     url: '702804/HELIANTHUS_ANNUUS_%28SUNFLOWER%29_OIL/',
+  },
+  {
+    name: 'Óleo de Amendoas',
+    inci: 'Prunus Amygdalus Dulcis (Sweet Almond) Oil',
+    hasIcon: true,
+    description:
+      '100% vegetal e prensado a frio, mantendo suas propriedades. Rico em vitaminas, nutre e hidrata a pele. ',
+    url: '707096-PRUNUS_AMYGDALUS_DULCIS_(SWEET_ALMOND)_OIL',
   },
   {
     name: 'Hidróxido de Magnésio',
     inci: 'Magnesium Hydroxide',
     description:
-      'Base para o conhecido Leite de Magnésia, que é utilizado como antiácido em nossa fórmula, regulando o PH do suor e assim impedindo a proliferação de bactérias nas axilas.',
+      'No desodorante, é um inibidor de bactérias que causam o mau cheiro. Ele regula o PH da axila, reduzindo a acidez e a proliferação de bactérias. Mais suave que o Bicarbonato de Sódio.',
     url: '720445/MAGNESIUM_HYDROXIDE/',
   },
   {
@@ -145,21 +155,21 @@ export default [
     showHome: true,
     hasIcon: true,
     description:
-      'Reverenciada pelos egípcios, maias e norte-americanos nativos por suas propriedades terapêuticas, o óleo de Rosa Mosqueta oferece uma forma natural de combater rugas, desacelerar o envelhecimento e se atingir uma pele saudável. Estudos clínicos demonstram que ela também tem qualidades antibacterianas, antivirais e antifúngicas.',
+      'É uma forma natural de combater rugas, desacelerar o envelhecimento e de ter uma pele saudável. Rico em ácidos graxos nutritivos que contribuem para regeneração das células e tecidos.',
     url: '705579/ROSA_CANINA_%28DOG_ROSE%29_FRUIT_OIL/',
   },
   {
     name: 'Palmarosa',
     inci: 'Cymbopogon Martini',
     description:
-      'O óleo essencial de Palmarosa é extraído por hidratação ou destilação a vapor, que produz um óleo  com um odor doce, floral e rosado, com nuances frutadas, especiadas e amadeiradas. Alguns dos usos do óleo de palmarosa incluem: tratamento de manchas na pele, revitalização e manutenção da flexibilidade e outros danos causados pelo envelhecimento, tratamento de fungos, entre outros.',
+      'O óleo essencial de Palmarosa é extraído por hidratação ou destilação a vapor, que produz um óleo com um odor doce, floral e rosado, com nuances frutadas, especiadas e amadeiradas. Alguns dos usos do óleo de palmarosa incluem: tratamento de manchas na pele, revitalização e manutenção da flexibilidade e outros danos causados pelo envelhecimento, tratamento de fungos, entre outros.',
     url: '701749/CYMBOPOGON_MARTINI_%28PALMAROSA%29/',
   },
   {
     name: 'Copaíba',
     inci: 'Copaifera Officinalis Resin',
     description:
-      'O óleo de copaíba foi apelidado de "bálsamo da Amazônia" devido às suas qualidades. Possui componentes de ação anti-inflamatória, antifúngica, antiedêmica, antiviral, antiúlcera, entre outros... ufa!',
+      'O óleo de copaíba foi apelidado de "bálsamo da Amazônia" devido às suas qualidades. Dentre elas, adoramos, o poder de hidratação e ação anti-inflamatória.',
     url: '701635/COPAIFERA_OFFICINALIS_%28BALSAM_COPAIBA%29_RESIN/',
   },
   {
@@ -167,7 +177,7 @@ export default [
     inci: 'Coffea Arabica',
     hasIcon: true,
     description:
-      'Possui propriedades tonificantes, suavizantes e antissépticas e é muito utilizado em tratamentos de beleza corporais e faciais. É rico em ácidos graxos insaturados, como o ácido linoleico, que atua na cura de dermatites e alívio de eczemas. Os ácidos graxos promovem, também, a hidratação da pele e proteção contra raios UV.',
+      'Possui propriedades tonificantes, suavizantes e antissépticas e é muito utilizado em tratamentos de beleza corporais e faciais. Os ácidos graxos promovem, também, a hidratação da pele e proteção contra raios UV.',
     url: '701591/COFFEA_ARABICA_%28ARABIAN_COFFEE%29/',
   },
   {
@@ -175,15 +185,14 @@ export default [
     inci: 'Olea Europaea',
     showHome: true,
     hasIcon: true,
-    description:
-      'Rico em vitaminas A, D, E e K e antioxidantes, o azeite colabora para o combate aos radicais livres. Sendo assim, ajuda na prevenção do envelhecimento precoce da pele.',
+    description: 'Hidrata e nutre cabelo e couro cabeludo.',
     url: '704248/OLEA_EUROPAEA_%28OLIVE%29/',
   },
   {
     name: 'Lactato de Sódio',
     inci: 'Sodium Lactate',
     description:
-      'O lactato de sódio é o sal de sódio do ácido láctico e apresenta um sabor salino suave. É produzido por fermentação de uma fonte de açúcar, como milho ou beterraba.',
+      'O lactato de sódio é produzido por fermentação de uma fonte de açúcar, como milho ou beterraba.',
     url: '706084/SODIUM_LACTATE/',
   },
   {
@@ -192,28 +201,28 @@ export default [
     showHome: true,
     hasIcon: true,
     description:
-      'Simplesmente a casca de uma árvore (Juazeiro) em pó! O juá é um antiséptico e saponáceo natural.',
+      'Simplesmente a casca de uma árvore (Juazeiro) em pó! O juá é um antiséptico e saponáceo natural. Excelente para higiene bucal, combatendo cáries e placa.',
     url: '724946/ZIZYPHUS_JOAZEIRO_BARK_EXTRACT/',
   },
   {
     name: '',
     inci: 'Caprylhydroxamic Acid',
     description:
-      'É ideal para produtos de cuidados pessoais onde uma reivindicação sem paraben ou livre de conservante é necessária. Pode ser usado como uma alternativa às misturas conservantes tradicionais que são vistas como indesejáveis ​​pelo consumidor. ',
+      'É ideal para produtos de cuidados pessoais onde uma reivindicação sem paraben ou livre de conservante é necessária. Pode ser usado como uma alternativa às misturas conservantes tradicionais que são vistas como indesejáveis ​​pelo consumidor.',
     url: '862573/CAPRYLHYDROXAMIC_ACID/',
   },
   {
     name: '',
     inci: 'Caprylyl Glycol',
     description:
-      'É um álcool derivado do ácido caprílico, que é  um ácido gordo natural encontrado nos óleos de palma e de coco. Considerado um conservante "preservative free", é uma ótima opção para sua cosmética natural e orgânica.',
+      'É um derivado dos óleos de palma e de coco. É considerado um conservante "preservative free".',
     url: '701067/CAPRYLYL_GLYCOL/',
   },
   {
     name: 'Etilhexilglicerina',
     inci: 'Ethylhexylglycerin',
     description:
-      'A etilhexilglicerina, ou octoxiglicerina, é um éter glicerílico que é comumente usado como parte de um sistema conservante em preparações cosméticas, comumente usado em cosméticos livres de parabeno.',
+      'Conservante que vem de grãos e plantas. É multifuncional, atuando como hidratante e desodorante.',
     url: '702352/ETHYLHEXYLGLYCERIN/',
   },
   {
@@ -239,7 +248,6 @@ export default [
   {
     name: 'Argila Verde',
     inci: 'Montmorillonite',
-    showHome: true,
     description:
       'A argila é famosa pelos seus benefícios no cuidado com a pele, sendo comumente usada em tratamentos há milhares de anos. Os egípcios, por exemplo, já usavam esse componente nas suas mumificações.',
     url: '704015/MONTMORILLONITE/',
