bf2a60735d1c491853447219a642922d4daf0987
Gustavo Guichard <gustavoguichard@gmail.com>
2019-12-13 14:39:43 -0300

    Only add Host if there's a EXT_API_DOMAIN

--- pages/api/proxy/[to].ts
+++ pages/api/proxy/[to].ts
@@ -31,26 +31,26 @@ const keysWithLodash = (_: string, k: string) => k.startsWith('_')
 export default async (req: any, res: Http2ServerResponse) => {
   const endpoint = req.url.replace('/api/proxy/', '').replace('::', '/')
   const { host } = absoluteUrl(req)
-  console.log('host:', host)
 
   try {
     if (endpoint) {
       const encodedCookies = omitCookies(req.headers.cookie, keysWithLodash)
       const url = `${process.env.API_IP}${endpoint}`
-      console.log('url:', url)
+      const headersBase = {
+        Accept: 'application/json',
+        cookie: encodedCookies,
+      }
+      const headers = process.env.EXT_API_DOMAIN
+        ? {
+            ...headersBase,
+            Host: process.env.EXT_API_DOMAIN,
+          }
+        : headersBase
       const response = await fetch(url, {
-        headers: {
-          Accept: 'application/json',
-          cookie: encodedCookies,
-          Host: process.env.EXT_API_DOMAIN || host,
-        },
+        headers,
         method: req.method,
         credentials: 'include',
       })
-      console.log(
-        'process.env.EXT_API_DOMAIN || host:',
-        process.env.EXT_API_DOMAIN || host,
-      )
       const cookie = response.headers.get('Set-Cookie') || ''
       res.writeHead(response.status, { Biscuit: cookie })
       const isSuccess = response.status < 400
