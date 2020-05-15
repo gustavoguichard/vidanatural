63f6070cae3d6d860751d683b356d7fd56965c92
Gustavo Guichard <gustavoguichard@gmail.com>
2020-05-15 20:16:58 -0300

    SSG for pagina/[pageId]

--- pages/pagina/[pageId].js
+++ pages/pagina/[pageId].js
@@ -1,29 +1,43 @@
+import get from 'lodash/get'
 import { Grid, Typography } from '@material-ui/core'
+import ErrorPage from 'pages/404'
 import Layout from 'src/ui/Layout'
 import Hero from 'src/components/Hero'
 import PaperContent from 'src/ui/PaperContent'
 import theme from 'src/ui/theme'
 import api from 'utils/api'
 
-const ContentPage = ({ page }) => (
-  <Layout title={page.title} seo={{ description: page.description }}>
-    <Hero size="small" background="/static/images/capa-pb.jpg">
-      <Typography variant="h2">{page.title}</Typography>
-      <Typography variant="body1" css={{ margin: theme.spacing(3) }}>
-        {page.description}
-      </Typography>
-    </Hero>
-    <PaperContent>
-      <Grid container justify="center">
-        <Grid item xs={12} md={7}>
-          <div dangerouslySetInnerHTML={{ __html: page.body }} />
+const ContentPage = ({ page }) =>
+  get(page, 'id') ? (
+    <Layout title={page.title} seo={{ description: page.description }}>
+      <Hero size="small" background="/static/images/capa-pb.jpg">
+        <Typography variant="h2">{page.title}</Typography>
+        <Typography variant="body1" css={{ margin: theme.spacing(3) }}>
+          {page.description}
+        </Typography>
+      </Hero>
+      <PaperContent>
+        <Grid container justify="center">
+          <Grid item xs={12} md={7}>
+            <div dangerouslySetInnerHTML={{ __html: page.body }} />
+          </Grid>
         </Grid>
-      </Grid>
-    </PaperContent>
-  </Layout>
-)
+      </PaperContent>
+    </Layout>
+  ) : (
+    <ErrorPage />
+  )
 
-export async function getServerSideProps({ params }) {
+export async function getStaticPaths() {
+  return {
+    paths: ['termos-e-condicoes'].map((pageId) => ({
+      params: { pageId },
+    })),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params }) {
   const { pageId } = params
   const page = await api.listPage(pageId)
   return { props: { page } }
