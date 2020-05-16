ba3143421da321976878db83f9eb9be636822654
Gustavo Guichard <gustavoguichard@gmail.com>
2020-05-15 22:20:40 -0300

    Remove unnecessary config from next

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 const withPlugins = require('next-compose-plugins')
 const purgeCss = require('next-purgecss')
-// const offline = require('next-offline')
 const optimizedImages = require('next-optimized-images')
-const dotenvLoad = require('dotenv-load')
 const sourceMaps = require('@zeit/next-source-maps')
 
-dotenvLoad()
-
 const nextConfig = {
-  webpack: (config) => {
-    // eslint-disable-next-line
-    config.node = {
-      fs: 'empty',
-      child_process: 'empty',
-      readline: 'empty',
-    }
-    return config
-  },
   purgeCss: {
     whitelist: () => ['MuiPaper-root', 'MuiPaper-rounded', 'MuiFormLabel-root'],
   },
