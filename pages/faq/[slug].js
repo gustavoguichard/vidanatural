a0f88b2de805b30dd2d279f2c53b9b39f9235b79
Gustavo Guichard <gustavoguichard@gmail.com>
2020-05-17 15:12:06 -0300

    Remove fallback from faq items

--- pages/faq/[slug].js
+++ pages/faq/[slug].js
@@ -1,22 +1,17 @@
 import get from 'lodash/get'
-import { useRouter } from 'next/router'
 import { Typography } from '@material-ui/core'
-import ErrorPage from 'pages/404'
 import SinglePageLayout from 'src/ui/SinglePageLayout'
 import CTAButton from 'src/components/CTAButton'
 import { RichText } from 'prismic-reactjs'
 import theme from 'src/ui/theme'
 import * as cms from 'utils/cms'
 
-const FaqPage = ({ data, error }) => {
-  const { isFallback } = useRouter()
-  const title = isFallback ? 'Carregando...' : get(data, 'question.0.text')
-  return error ? (
-    <ErrorPage />
-  ) : (
+const FaqPage = ({ data }) => {
+  const title = get(data, 'question.0.text')
+  return (
     <SinglePageLayout title={title}>
       <Typography variant="h2">{title}</Typography>
-      {data && !isFallback && <RichText render={data.answer} />}
+      <RichText render={data.answer} />
       <CTAButton
         center={false}
         href="/faq"
@@ -32,14 +27,14 @@ export async function getStaticPaths() {
   const items = await cms.allByTypeAndTags('faq_item')
   return {
     paths: items.map((item) => ({ params: { slug: item.uid } })),
-    fallback: true,
+    fallback: false,
   }
 }
 
 export async function getStaticProps({ params }) {
   const { slug } = params
   const props = await cms.getBySlug('faq_item', slug)
-  return props ? { props } : { props: { error: true } }
+  return { props }
 }
 
 export default FaqPage
