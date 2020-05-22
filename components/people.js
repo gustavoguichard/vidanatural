ac40f8a43c001bd0b2e956281d5e104a9b2ac1c7
Gustavo Guichard <gustavoguichard@gmail.com>
2020-05-21 21:57:48 -0300

    Remove testimonials popup

--- components/people.js
+++ components/people.js
@@ -1,26 +1,17 @@
-import { useState, memo } from 'react'
+import { memo } from 'react'
 import shuffle from 'lodash/shuffle'
-import { Container, Dialog, Paper } from '@material-ui/core'
+import { Container, Paper } from '@material-ui/core'
 
 import { useProcessOnce } from 'lib/hooks'
 import theme from 'lib/theme'
 
 import Masonry from 'components/masonry'
 import Person from 'components/person'
-import Testimonial from 'components/testimonial'
 
 import testimonials from 'data/testimonials'
 
 const People = () => {
-  const [current, setCurrent] = useState(null)
   const shuffled = useProcessOnce(shuffle, testimonials)
-  const onOpen = (index) => {
-    const testimonial = shuffled[index]
-    setCurrent(testimonial)
-  }
-  const handleClose = () => setCurrent(null)
-  const isOpen = !!current
-
   return (
     <Container maxWidth="lg">
       <Paper
@@ -35,28 +26,9 @@ const People = () => {
       >
         <Masonry>
           {shuffled.map((testimonial, index) => (
-            <Person
-              onOpen={onOpen}
-              index={index}
-              key={index}
-              {...testimonial}
-            />
+            <Person key={index} {...testimonial} />
           ))}
         </Masonry>
-        <Dialog
-          scroll="body"
-          PaperComponent="div"
-          PaperProps={{
-            style: {
-              margin: 20,
-              maxWidth: 600,
-            },
-          }}
-          onClose={handleClose}
-          open={isOpen}
-        >
-          <Testimonial {...current} />
-        </Dialog>
       </Paper>
     </Container>
   )
--- components/person.js
+++ components/person.js
@@ -1,11 +1,11 @@
-import { memo, useRef } from 'react'
+import { memo } from 'react'
 import { Typography } from '@material-ui/core'
-import { Link, RemoveRedEye } from '@material-ui/icons'
+import { RemoveRedEye } from '@material-ui/icons'
 
 import theme from 'lib/theme'
 
 import Img from 'components/img'
-import VNLink from 'components/link'
+import Link from 'components/link'
 
 export const saturateOnHover = (time = '.3s') => ({
   filter: 'saturate(0)',
@@ -15,73 +15,49 @@ export const saturateOnHover = (time = '.3s') => ({
   },
 })
 
-const Testimonial = ({ onOpen, index, name, picture }) => {
-  const linkRef = useRef()
-
-  const onClick = (event) => {
-    if (event.currentTarget.dataset.id === 'link') {
-      event.stopPropagation()
-    } else {
-      event.preventDefault()
-      onOpen(index)
-    }
-  }
-
-  return (
-    <VNLink
-      href="/eu-uso/[name]"
-      as={`/eu-uso/${picture}`}
-      className="testimonial-item"
-      onClick={onClick}
+const Testimonial = ({ name, picture }) => (
+  <Link
+    href="/eu-uso/[name]"
+    as={`/eu-uso/${picture}`}
+    className="testimonial-item"
+  >
+    <figure
+      css={{
+        ...saturateOnHover(),
+        cursor: 'pointer',
+        margin: 0,
+        marginBottom: 1,
+        width: '100%',
+        display: 'flex',
+        position: 'relative',
+        flexDirection: 'column',
+        minHeight: 250,
+        paddingRight: 1,
+      }}
     >
-      <figure
+      <Img
+        css={{ width: '100%', objectFit: 'cover' }}
+        src={`/static/images/testimonials/${picture}.jpg`}
+        alt={name}
+      />
+      <div
         css={{
-          ...saturateOnHover(),
-          cursor: 'pointer',
-          margin: 0,
-          marginBottom: 1,
-          width: '100%',
+          backgroundColor: 'rgba(0,0,0,.5)',
+          bottom: 0,
+          left: 0,
+          padding: 12,
           display: 'flex',
-          position: 'relative',
-          flexDirection: 'column',
-          minHeight: 250,
-          paddingRight: 1,
+          position: 'absolute',
+          right: 0,
         }}
       >
-        <Img
-          css={{ width: '100%', objectFit: 'cover' }}
-          src={`/static/images/testimonials/${picture}.jpg`}
-          alt={name}
-        />
-        <div
-          css={{
-            backgroundColor: 'rgba(0,0,0,.5)',
-            bottom: 0,
-            left: 0,
-            padding: 12,
-            display: 'flex',
-            position: 'absolute',
-            right: 0,
-          }}
-        >
-          <Typography
-            css={{ flex: 1 }}
-            variant="subtitle1"
-            color="textSecondary"
-          >
-            {name}
-          </Typography>
-          <Link
-            onClick={onClick}
-            data-id="link"
-            ref={linkRef}
-            css={{ color: theme.palette.text.secondary, marginRight: 4 }}
-          />
-          <RemoveRedEye css={{ color: theme.palette.text.secondary }} />
-        </div>
-      </figure>
-    </VNLink>
-  )
-}
+        <Typography css={{ flex: 1 }} variant="subtitle1" color="textSecondary">
+          {name}
+        </Typography>
+        <RemoveRedEye css={{ color: theme.palette.text.secondary }} />
+      </div>
+    </figure>
+  </Link>
+)
 
 export default memo(Testimonial)
