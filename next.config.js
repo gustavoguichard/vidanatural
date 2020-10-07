aa5cd307fcc71742776c9d61baa96488bd46e8ee
Gustavo Guichard <gustavoguichard@gmail.com>
2020-10-07 18:52:13 -0300

    Remove unused next-purge call

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
-const purgeCss = require('next-purgecss')
 const sourceMaps = require('@zeit/next-source-maps')
 
 const nextConfig = {}
