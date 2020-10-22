2d3a13d6f0faa8174d96c64c63707aa6c0963512
Gustavo Guichard <gustavoguichard@gmail.com>
2020-10-22 11:03:59 -0300

    Add products endpoint for static-paths

--- lib/static-paths/produtos-uid.ts
+++ lib/static-paths/produtos-uid.ts
@@ -3,7 +3,7 @@ import { GetStaticPaths } from 'next'
 import api from 'lib/api'
 
 const getStaticPaths: GetStaticPaths = async () => {
-  const products = await api.vnda.search()
+  const products = await api.vnda.fetch('products')
   return {
     paths: products.map((p: any) => ({
       params: { slug: [p.slug, p.id].join('-') },
