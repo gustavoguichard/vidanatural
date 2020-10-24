5691dd1b25e6f33af6104f941d3b2d333d5f2615
Gustavo Guichard <gustavoguichard@gmail.com>
2020-10-24 00:54:38 -0300

    Copy fix

--- components/products/title.js
+++ components/products/title.js
@@ -1,11 +1,11 @@
 const ProductTitle = ({ product }) => {
   const [variant] = product.variants || [{}]
   return (
-    <div className="text-center lg:text-left leading-tight mb-3 lg:mb-2">
+    <div className="text-center lg:text-left mb-3 lg:mb-2">
       <h1 className="text-2xl sm:text-3xl font-bold tracking-tight">
         {product.name}
       </h1>
-      <p className="text-gray-700 uppercase text-sm mb-1">{variant.name}</p>
+      <p className="text-gray-700 uppercase text-sm my-1">{variant.name}</p>
     </div>
   )
 }
