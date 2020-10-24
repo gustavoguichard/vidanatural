42f38aeac785019c206dddc5b9e3f567b8df1c9c
Gustavo Guichard <gustavoguichard@gmail.com>
2020-10-24 00:56:51 -0300

    Leading fix

--- components/products/title.js
+++ components/products/title.js
@@ -2,7 +2,7 @@ const ProductTitle = ({ product }) => {
   const [variant] = product.variants || [{}]
   return (
     <div className="text-center lg:text-left mb-3 lg:mb-2">
-      <h1 className="text-2xl sm:text-3xl font-bold tracking-tight">
+      <h1 className="text-2xl sm:text-3xl font-bold leading-none tracking-tight">
         {product.name}
       </h1>
       <p className="text-gray-700 uppercase text-sm my-1">{variant.name}</p>
