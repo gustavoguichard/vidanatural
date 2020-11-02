6fed6547131fafa4dcd25a5b567932d89aefa46f
Gustavo Guichard <gustavoguichard@gmail.com>
2020-11-02 19:35:34 -0300

    💅

--- components/cart/header.js
+++ components/cart/header.js
@@ -6,7 +6,7 @@ const CartHeader = ({ actions, items }) => {
   const quantity = sumBy(items, 'quantity')
   return (
     <div className="sticky top-0 bg-white shadow-sm border-b text-sm py-1 px-4 flex items-center z-10">
-      <CloseButton onClick={actions.hideCart} />
+      <CloseButton className="-ml-3" onClick={actions.hideCart} />
       <p className="flex-grow ml-2 font-semibold">Seu carrinho</p>
       <span>{quantity} itens</span>
     </div>
