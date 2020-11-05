2cbb6d7819f3f84337e9b90cc70121135ff71ef7
Gustavo Guichard <gustavoguichard@gmail.com>
2020-11-05 11:55:31 -0300

    Refactor

---
+++ components/banner.js
@@ -0,0 +1,23 @@
+import CTAButton from 'components/cta-button'
+import Img from 'components/img'
+
+const Banner = ({ src, url, title, alwaysShow, subtitle, cta = 'Comprar' }) => (
+  <div className="relative flex md:flex-row flex-col min-h-screen">
+    <Img
+      className="max-h-screen object-cover md:w-7/12 lg:w-8/12"
+      src={src}
+      alt={title}
+      alwaysShow={alwaysShow}
+      css={{ minHeight: '70vh' }}
+    />
+    <div className="flex flex-col items-start md:items-stretch md:w-5/12 lg:w-4/12 bg-white md:border-l-8 border-gray-100 py-12 px-10 sm:px-12 justify-end text-black">
+      <h3 className="text-3xl lg:text-4xl xl:text-5xl tracking-tighter font-bold leading-none">
+        {title}
+      </h3>
+      {subtitle && <p className="mt-2 mb-4">{subtitle}</p>}
+      <CTAButton href={url}>{cta}</CTAButton>
+    </div>
+  </div>
+)
+
+export default Banner
--- components/banners.js
+++ components/banners.js
@@ -1,32 +1,22 @@
 import { resolveLink } from 'lib/domain'
 
-import CTAButton from 'components/cta-button'
-import Img from 'components/img'
+import Banner from 'components/banner'
 import Carousel from 'components/carousel'
 
-const HomeHero = ({ banners }) => (
+const HomeBanners = ({ banners }) => (
   <Carousel>
     {banners.map(({ data, id }, idx) => (
-      <div className="relative flex md:flex-row flex-col min-h-screen" key={id}>
-        <Img
-          className="max-h-screen object-cover md:w-7/12 lg:w-8/12"
-          src={data.image.url}
-          alt={data.title}
-          alwaysShow={idx === 0}
-          css={{ minHeight: '70vh' }}
-        />
-        <div className="flex flex-col items-start md:items-stretch md:w-5/12 lg:w-4/12 bg-white md:border-l-8 border-gray-100 py-12 px-10 sm:px-12 justify-end text-black">
-          <h3 className="text-3xl lg:text-4xl xl:text-5xl tracking-tighter font-bold leading-none">
-            {data.title}
-          </h3>
-          {data.subtitle && <p className="mt-2 mb-4">{data.subtitle}</p>}
-          <CTAButton href={resolveLink(data.link.url)}>
-            {data.button_text || 'Comprar'}
-          </CTAButton>
-        </div>
-      </div>
+      <Banner
+        key={id}
+        src={data.image.url}
+        title={data.title}
+        alwaysShow={idx === 0}
+        subtitle={data.subtitle}
+        url={resolveLink(data.link.url)}
+        cta={data.button_text}
+      />
     ))}
   </Carousel>
 )
 
-export default HomeHero
+export default HomeBanners
