92d9242b151e9d7fff7ffd93f1ad3ef6f9440d66
Gustavo Guichard <gustavoguichard@gmail.com>
2021-03-31 10:09:28 -0300

    Refactor

--- pages/api/cart/create.ts
+++ pages/api/cart/create.ts
@@ -4,20 +4,17 @@ import publicIp from 'public-ip'
 import vnda from 'lib/api/vnda2'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  let ip
-  let userAgent
+  let ip = '127.0.0.1'
+  let userAgent = 'not-found'
   try {
-    userAgent = req.headers['user-agent']
+    userAgent = req.headers['user-agent'] as string
   } catch (error) {
     console.log(error)
-    userAgent =
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
   }
   try {
     ip = await publicIp.v4()
   } catch (error) {
     console.log(error)
-    ip = '127.0.0.1'
   }
   const response = await vnda.fetcher('carts', 'POST', undefined, {
     'X-User-Agent': userAgent,
