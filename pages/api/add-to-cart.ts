b12a976aa502bdafaef0ededae98691b7ddb59b1
Gustavo Guichard <gustavoguichard@gmail.com>
2021-04-03 20:20:06 -0300

    Improve add-to-cart route to avoid errors

--- pages/api/add-to-cart.ts
+++ pages/api/add-to-cart.ts
@@ -3,18 +3,25 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import vnda from 'lib/api/vnda2'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const cartData = await vnda.fetch('cart/create')
-  const response = await vnda.fetcher(`carts/${cartData?.id}/items`, 'POST', {
-    quantity: 1,
-    sku: req.body.sku,
-  })
+  try {
+    const cartData = await vnda.fetch('cart/create')
+    const response = await vnda.fetcher(`carts/${cartData?.id}/items`, 'POST', {
+      quantity: 1,
+      sku: req.body.sku,
+    })
 
-  if (response.data) {
-    res.redirect(`${process.env.API_IP}pagamento/${cartData?.token}`)
-  } else {
+    if (response.data) {
+      res.redirect(301, `${process.env.API_IP}pagamento/${cartData?.token}`)
+    } else {
+      res.send({
+        status: 500,
+        error: 'Não foi possível adicionar os produtos ao carrinho',
+      })
+    }
+  } catch (error) {
     res.send({
       status: 500,
-      error: 'Não foi possível adicionar os produtos ao carrinho',
+      error,
     })
   }
 }
