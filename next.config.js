d35c6f82e4163f40426e88780c18d5b2a1947b13
Gustavo Guichard <gustavoguichard@gmail.com>
2021-04-04 11:16:33 -0300

    Don't compress next

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 
 const nextConfig = {
+  compress: false,
   async rewrites() {
     return [
       {
