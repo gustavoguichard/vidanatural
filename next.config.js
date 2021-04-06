1062d25b28e5ba82f08a0a24b1cd8f6dde4bf783
Gustavo Guichard <gustavoguichard@gmail.com>
2021-04-06 12:11:44 -0300

    Next.config with only compress: false

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
-const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
+// const withPlugins = require('next-compose-plugins')
+// const optimizedImages = require('next-optimized-images')
 
-const nextConfig = {
-  compress: false,
-  async rewrites() {
-    return [
-      {
-        source: '/produtos/:slug',
-        destination: '/produto/:slug',
-      },
-    ]
-  },
-}
+// const nextConfig = {
+//   compress: false,
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/produtos/:slug',
+//         destination: '/produto/:slug',
+//       },
+//     ]
+//   },
+// }
 
-module.exports = withPlugins(
-  [[optimizedImages, { optimizeImagesInDev: true }]],
-  nextConfig,
-)
+// module.exports = withPlugins(
+//   [[optimizedImages, { optimizeImagesInDev: true }]],
+//   nextConfig,
+// )
+
+module.exports = { compress: false }
