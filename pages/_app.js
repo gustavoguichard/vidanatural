135e6c368cdce9cce9f1b66eee559cbe386bdd98
Gustavo Guichard <gustavoguichard@gmail.com>
2021-05-20 22:12:22 -0300

    Add Clarity code

--- pages/_app.js
+++ pages/_app.js
@@ -29,6 +29,17 @@ const VidaNatural = ({ pageProps, Component }) => {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
         />
+        <script
+          defer
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_TOKEN}");`,
+          }}
+        />
       </Head>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
