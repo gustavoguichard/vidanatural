d994bcaca5007f7bef9b9c974fc9865459b79dc9
Gustavo Guichard <gustavoguichard@gmail.com>
2021-09-09 05:06:24 -0300

    Readjust notifications in desktop

--- components/notifications/index.tsx
+++ components/notifications/index.tsx
@@ -10,14 +10,14 @@ const Notifications = () => {
       aria-live="assertive"
       className="fixed inset-0 z-50 flex flex-col justify-between grid-cols-1 grid-rows-2 gap-4 px-4 py-6 pointer-events-none sm:grid sm:grid-cols-2 sm:p-6 sm:items-start"
     >
-      <div className="relative flex flex-col items-start w-full gap-4">
+      <div className="relative flex flex-col items-start w-full gap-4 sm:h-full">
         {notifications
           .filter(({ position }) => position === 'top-left' || !position)
           .map((n) => (
             <Message key={n.id} notification={n} />
           ))}
       </div>
-      <div className="relative flex flex-col items-end w-full gap-4">
+      <div className="relative flex flex-col items-end w-full gap-4 sm:h-full">
         {notifications
           .filter(({ position }) => position === 'top-right')
           .map((n) => (
@@ -25,14 +25,14 @@ const Notifications = () => {
           ))}
       </div>
       <div className="flex-grow sm:hidden" />
-      <div className="relative flex flex-col-reverse items-start w-full gap-4">
+      <div className="relative flex flex-col-reverse items-start w-full gap-4 sm:h-full">
         {notifications
           .filter(({ position }) => position === 'bottom-left')
           .map((n) => (
             <Message key={n.id} notification={n} />
           ))}
       </div>
-      <div className="relative flex flex-col-reverse items-end w-full gap-4">
+      <div className="relative flex flex-col-reverse items-end w-full gap-4 sm:h-full">
         {notifications
           .filter(({ position }) => position === 'bottom-right')
           .map((n) => (
