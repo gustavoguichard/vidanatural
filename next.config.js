ae7c5589bd96d7064ba84fcf05f0a52d07a4485e
Gustavo Guichard <gustavoguichard@gmail.com>
2021-09-08 22:37:04 -0300

    Add security headers

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ module.exports = {
   async redirects() {
     return [{ source: '/link-da-caixa', destination: '/', permanent: false }]
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
   images: {
     domains: [
       'images.prismic.io',
@@ -34,3 +42,34 @@ module.exports = {
     return config
   },
 }
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.facebook.net *.googletagmanager.com *.clarity.ms *.google-analytics.com *.googleadservices.com *.doubleclick.net;
+  child-src *.youtube.com *.google.com;
+  style-src 'self' 'unsafe-inline' rsms.me;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self' rsms.me;
+`
+
+// Copied from https://github.com/leerob/leerob.io/blob/main/next.config.js
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+  { key: 'X-Frame-Options', value: 'DENY' },
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'X-DNS-Prefetch-Control', value: 'on' },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'geolocation=()',
+  },
+]
